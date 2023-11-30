dbca45473bc76331b4f2b9c30716801ae165b94e
rosso101 <victorprivv101@gmail.com>
2023-11-30 14:47:42 +0300

    completed partly

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,10 @@
 class Vehicle:
-    pass
+  def __init__(self, wheel_size, wheel_number):
+    self.wheel_size = wheel_size
+    self.wheel_number = wheel_number
+
+    def go(self):
+      return"stutututu!!"
+    
+    def fill_up_tank(self):
+      return"fulling up my nissan GTR R35 THAT GOES VERY VERY FAST"
